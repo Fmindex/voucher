9c00eddb055d27379e5aca0c2466df4ab2c47a4a
burin-n <ekkalakfm@gmail.com>
2018-05-30 01:34:47 +0700

    init

---
+++ public/javascripts/global.js
@@ -0,0 +1,222 @@
+// Userlist data array for filling in info box
+var voucherListData = [];
+
+// DOM Ready =============================================================
+$(document).ready(function() {
+
+    // Populate the user table on initial page load
+    populateTable();
+
+    // Username link click
+    $('#voucherList table tbody').on('click', 'td a.linkshowuser', showVoucherInfo);
+
+    // Add User button click
+    $('#btnAddVoucher').on('click', addVoucher);
+    $('#btnEditVoucher').on('click', editVoucher);
+
+    // Delete User link click
+    $('#voucherList table tbody').on('click', 'td a.linkdeleteuser', deleteVoucher);
+
+});
+
+// Functions =============================================================
+
+// Fill table with data
+function populateTable() {
+
+    // Empty content string
+    var tableContent = '';
+
+    // jQuery AJAX call for JSON
+    $.getJSON( '/voucher/voucherList/', function( data ) {
+
+        // Stick our user data array into a userlist variable in the global object
+        voucherListData = data;
+
+        // For each item in our JSON, add a table row and cells to the content string
+        $.each(data, function(){
+            tableContent += '<tr>';
+            tableContent += '<td><a href="#" class="linkshowuser" rel="' + this.name + '" myid="' + this._id + '" myimg="' + this.img + '" myclick="' + this.click + '" myclick2="' + this.click2 + '" title="Show Details">' + this.name + '</a></td>';
+            tableContent += '<td>' + this.img + '</td>';
+            tableContent += '<td>' + this.space + '</td>';
+            tableContent += '<td>' + this.click + '</td>';
+            tableContent += '<td>' + this.click2 + '</td>';
+            tableContent += '<td>' + this.last_update + '</td>';
+            tableContent += '<td><a href="#" class="linkdeleteuser" rel="' + this._id + '">delete</a></td>';
+            tableContent += '</tr>';
+        });
+
+        // Inject the whole content string into our existing HTML table
+        $('#voucherList table tbody').html(tableContent);
+    });
+};
+
+// Show User Info
+var thisVoucherName, thisVoucherId, thisVoucherImg, thisVoucherClick;
+function showVoucherInfo(event) {
+
+    // Prevent Link from Firing
+    event.preventDefault();
+
+    // Retrieve username from link rel attribute
+    thisVoucherName = $(this).attr('rel');
+    thisVoucherId = $(this).attr('myid');
+    thisVoucherImg = $(this).attr('myimg');
+    thisVoucherClick = $(this).attr('myclick');
+    thisVoucherClick2 = $(this).attr('myclick2');
+    console.log(thisVoucherId);
+    // Get Index of object based on id value
+    var arrayPosition = voucherListData.map(function(arrayItem) { return arrayItem.name; }).indexOf(thisVoucherName);
+
+    // Get our User Object
+    var thisVoucherObject = voucherListData[arrayPosition];
+
+    //Populate Info Box
+    $('#voucherInfoName').text(thisVoucherObject.name);
+    $('#voucherInfoImg').text(thisVoucherObject.img);
+    $('#voucherInfoSpace').text(thisVoucherObject.space);
+
+};
+
+// Add Voucher
+function addVoucher(event) {
+    event.preventDefault();
+
+    // Super basic validation - increase errorCount variable if any fields are blank
+    var errorCount = 0;
+    $('#addVoucher input').each(function(index, val) {
+        if($(this).val() === '') { errorCount++; }
+    });
+
+    // Check and make sure errorCount's still at zero
+    if(errorCount === 0) {
+
+        // If it is, compile all user info into one object
+        var newVoucher = {
+            'name': $('#addVoucher fieldset input#inputVoucherName').val(),
+            'img': $('#addVoucher fieldset input#inputVoucherImg').val(),
+            'space': $('#addVoucher fieldset input#inputVoucherSpace').val(),
+            'click': 0,
+            'click2': 0,
+        }
+
+        // Use AJAX to post the object to our adduser service
+        $.ajax({
+            type: 'POST',
+            data: newVoucher,
+            url: '/voucher/addVoucher/',
+            dataType: 'JSON'
+        }).done(function( response ) {
+
+            // Check for successful (blank) response
+            if (response.msg === '') {
+
+                // Clear the form inputs
+                $('#addVoucher fieldset input').val('');
+
+                // Update the table
+                populateTable();
+
+            }
+            else {
+
+                // If something goes wrong, alert the error message that our service returned
+                alert('Error: ' + response.msg);
+
+            }
+        });
+    }
+    else {
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
+};
+
+// Delete User
+function deleteVoucher(event) {
+
+    event.preventDefault();
+
+    // Pop up a confirmation dialog
+    var confirmation = confirm('Are you sure you want to delete this user?');
+
+    // Check and make sure the user confirmed
+    if (confirmation === true) {
+        // If they did, do our delete
+        $.ajax({
+            type: 'DELETE',
+            url: '/voucher/deleteVoucher/' + $(this).attr('rel')
+        }).done(function( response ) {
+
+            // Check for a successful (blank) response
+            if (response.msg === '') {
+            }
+            else {
+                alert('Error: ' + response.msg);
+            }
+
+            // Update the table
+            populateTable();
+
+        });
+
+    }
+    else {
+
+        // If they said no to the confirm, do nothing
+        return false;
+
+    }
+
+};
+
+//Edit Voucher
+function editVoucher(event) {
+    event.preventDefault();
+
+    // Super basic validation - increase errorCount variable if any fields are blank
+    var errorCount = 0;
+    $('#editVoucher input').each(function(index, val) {
+        if($(this).val() === '') { errorCount++; }
+    });
+
+    // Check and make sure errorCount's still at zero
+    if(errorCount === 0) {
+
+        // If it is, compile all user info into one object
+        var newVoucher = {
+            'name': thisVoucherName,
+            'img': thisVoucherImg,
+            'space': $('#editVoucher fieldset2 input#inputSpace').val(),
+            'click': thisVoucherClick,
+            'click2': thisVoucherClick2,
+        }
+        console.log(newVoucher);
+        console.log(thisVoucherId);
+        // Use AJAX to post the object to our adduser service
+        $.ajax({
+            type: 'PUT',
+            data: newVoucher,
+            url: '/voucher/editvoucher/' + thisVoucherId,
+            dataType: 'JSON'
+        }).done(function( response ) {
+
+            // Check for successful (blank) response
+            
+
+                // Clear the form inputs
+                $('#editVoucher fieldset2 input#inputSpace').val('');
+                $('#voucherInfoSpace').text(newVoucher.space);
+
+                // Update the table
+                populateTable();
+            
+        });
+    }
+    else {
+        // If errorCount is more than 0, error out
+        alert('Please fill in all fields');
+        return false;
+    }
+};
---
+++ routes/voucher.js
@@ -0,0 +1,57 @@
+var express = require('express');
+var router = express.Router();
+var ObjectId = require('mongodb').ObjectId; 
+var moment = require('moment');
+/*
+ * GET userlist.
+ */
+router.get('/voucherList', function(req, res) {
+    var db = req.db;
+    var collection = db.get('voucher');
+    collection.find({},{},function(e,docs){
+        res.json(docs);
+    });
+});
+
+/*
+ * POST to adduser.
+ */
+router.post('/addvoucher', function(req, res) {
+    var time = moment().utcOffset('+0700').format('MMMM Do YYYY, HH:mm:ss');
+    var db = req.db;
+    var collection = db.get('voucher');
+    req.body.last_update = time;
+    collection.insert(req.body, function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+/*
+ * DELETE to deleteuser.
+ */
+router.delete('/deletevoucher/:id', function(req, res) {
+    var db = req.db;
+    var collection = db.get('voucher');
+    var userToDelete = req.params.id;
+    collection.remove({ '_id' : userToDelete }, function(err) {
+        res.send((err === null) ? { msg: '' } : { msg:'error: ' + err });
+    });
+});
+
+router.put('/editvoucher/:id', function(req, res) {
+    var time = moment().utcOffset('+0700').format('MMMM Do YYYY, HH:mm:ss');
+    var db = req.db;
+    var userToEdit = req.params.id;
+    var collection = db.get('voucher');
+    req.body.last_update = time;
+    collection.findOneAndUpdate({ _id: userToEdit }, req.body ).then(function(err, result){
+        res.send(
+            (err === null) ? { msg: '' } : { msg: err }
+        );
+    });
+});
+
+
+module.exports = router;
